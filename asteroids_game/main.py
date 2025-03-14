13efda2b9668f7ae1505e6519cb6da46cc62730e
motelian <momotelian@gmail.com>
2025-03-14 15:16:00 -0700

    base class for the game objects

--- asteroids_game/main.py
+++ asteroids_game/main.py
@@ -7,15 +7,20 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill(color="black")
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(0.01)/1000
+        print(dt)
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
