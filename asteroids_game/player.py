b8efec002f07ee379913d3f0a23a362a89005a32
motelian <momotelian@gmail.com>
2025-03-17 09:45:45 -0700

    add lag to shots

--- asteroids_game/player.py
+++ asteroids_game/player.py
@@ -7,11 +7,13 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shoot_timer = 0
 
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
 
     def update(self, dt):
+        self.shoot_timer -= dt
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
@@ -41,5 +43,8 @@ class Player(CircleShape):
         return [a, b, c]
 
     def shoot(self):
+        if self.shoot_timer > 0:
+            return
+        self.shoot_timer = PLAYER_SHOOT_COOLDOWN
         new_shot = Shot(self.position.x, self.position.y)
         new_shot.velocity = PLAYER_SHOOT_SPEED * pygame.Vector2(0,1).rotate(self.rotation)
