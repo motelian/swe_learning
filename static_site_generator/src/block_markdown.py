aa785728414a8d1a8f79536077b7c91ea2bccd97
motelian <momotelian@gmail.com>
2025-04-04 20:10:44 -0700

    convert markdown to html nodes and add unit tests

--- static_site_generator/src/block_markdown.py
+++ static_site_generator/src/block_markdown.py
@@ -1,4 +1,9 @@
-from textnode import TextType, TextNode
+from argparse import PARSER
+from textnode import TextType, TextNode, text_node_to_html_node
+from inline_markdown import (
+    text_to_textnodes
+)
+from htmlnode import ParentNode, LeafNode
 from enum import Enum
 import re
 
@@ -13,13 +18,65 @@ class BlockType(Enum):
     CODE = "code"
 
 
+def markdown_to_html_node(markdown):
+    # converts a full markdown document into a single parent HTMLNode
+
+    # split the markdown into blocks
+    blocks = markdown_to_blocks(markdown)
+    html_body_nodes = []
+    for block in blocks:
+        block_type = block_to_block_type(block)
+
+        if block_type == BlockType.PARAGRAPH:
+            lines = block.split("\n")
+            paragraph = " ".join(lines)
+            children = text_to_children(paragraph)
+            html_body_nodes.append(ParentNode("p", children))
+
+        if block_type == BlockType.HEADING:
+            match_pattern = re.match(r"(^#{1,7})(.*)", block)
+            md_syntax = match_pattern.group(1)
+            raw_text = match_pattern.group(2).strip()
+            children = text_to_children(raw_text)
+            html_body_nodes.append(ParentNode(f"h{len(md_syntax)}", children))
+
+        if block_type == BlockType.QUOTE:
+            raw_text = " ".join(map(lambda x: x[1:].strip(), block.split("\n")))
+            children = text_to_children(raw_text)
+            html_body_nodes.append(ParentNode("blockquote", children))
+
+        if block_type == BlockType.ORDERED_LIST:
+            block_nodes = []
+            for line in block.split("\n"):
+                line_text = line[2:].strip()
+                item_children = text_to_children(line_text)
+                block_nodes.append(ParentNode("li", item_children))
+            html_body_nodes.append(ParentNode("ol", block_nodes))
+
+        if block_type == BlockType.UNORDERED_LIST:
+            block_nodes = []
+            for line in block.split("\n"):
+                line_text = line[1:].strip()
+                item_children = text_to_children(line_text)
+                block_nodes.append(ParentNode("li", item_children))
+            html_body_nodes.append(ParentNode("ul", block_nodes))
+
+        if block_type == BlockType.CODE:
+            code = block.split("```")[1]
+            html_body_nodes.append(
+                ParentNode("pre", [text_node_to_html_node(TextNode(code, TextType.CODE))])
+            )
+    return ParentNode("div", html_body_nodes)
+
+
+def text_to_children(text):
+    textnodes = text_to_textnodes(text)
+    return list(map(text_node_to_html_node, textnodes))
+
+
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
     return list(filter(None, map(lambda x:x.strip(), blocks)))
-
-def markdown_to_html_node(markdown):
-    # converts a full markdown document into a single parent HTMLNode
-    pass
 
 def block_to_block_type(block):
 
@@ -36,19 +93,21 @@ def block_to_block_type(block):
         BlockType.UNORDERED_LIST: r"^- "
     }
 
+    # check if the block is one of headings, quote, unordered list types
     for k, v in pattern_blocktype.items():
         if all(map(is_match(v), lines)):
             return k
 
+    # check if the block is a code
     if block.startswith("```") and block.endswith("```"):
         return BlockType.CODE
 
+    # check if the block is an ordered list
     is_ordered_list = 1
     for i, l in enumerate(lines):
         if not l.startswith(f"{i+1}. "):
             is_ordered_list = 0
             break
-
     if is_ordered_list == 1:
         return BlockType.ORDERED_LIST
 
